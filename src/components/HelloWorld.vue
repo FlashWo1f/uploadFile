cea9f1b0fcf7d7e0c1632283bd4e8be471d68291
libin <1064048947@qq.com>
2020-07-14 18:24:52 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,56 +1,82 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+   <div>
+    <input type="file" @change="handleFileChange" />
+    <el-button @click="handleUpload">上传</el-button>
   </div>
 </template>
 
 <script>
+import { request } from '../api'
+// 切片大小
+const SIZE = 0.1 * 1024 * 1024
+// const SIZE = 10 * 1024 * 1024
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'helloworld',
+  data: () => ({
+    container: {
+      file: null
+    },
+    data: []
+  }),
+  methods: {
+     handleFileChange(e) {
+      const [file] = e.target.files;
+      // console.log("?", this.$data, this.$options.data(), Object.assign(this.$data, this.$options.data()))
+      if (!file) return;
+      Object.assign(this.$data, this.$options.data());
+      this.container.file = file;
+    },
+    async handleUpload() {
+      if (!this.container.file) return;
+      const fileChunkList = this.createFileChunk(this.container.file)
+      this.data = fileChunkList.map(({ file }, idx) => ({
+        chunk: file,
+        hash: this.container.file.name + "-" + idx // 文件名 + 数组下标
+      }))
+      await this.uploadChunks()
+    },
+    // 生成文件切片
+    createFileChunk(file, size = SIZE) {
+      const fileChunkList = []
+      let cur = 0 
+      while(cur < file.size) {
+        fileChunkList.push({ file: file.slice(cur, cur + size) })
+        cur += size;
+      }
+      return fileChunkList
+    },
+    // 上传切片
+    async uploadChunks() {
+      const requestList = this.data.map(({chunk, hash}) => {
+        const formData = new FormData()
+        formData.append("chunk", chunk)
+        formData.append("hash", hash)
+        formData.append("filename", this.container.file.name)
+        return { formData }
+      })
+      .map(async ({ formData }) =>
+        request({
+          url: "http://localhost:3000",
+          data: formData
+        })
+      )
+      // 并发切片
+      await Promise.all(requestList)
+      // 合并切片
+      await this.mergeRequest()
+    },
+    async mergeRequest(size = SIZE) {
+      await request({
+        url: "http://localhost:3000/merge",
+        headers: {
+          "content-type": "application/json"
+        },
+        data: JSON.stringify({
+          filename: this.container.file.name,
+          size
+        })
+      })
+    }
   }
-}
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="stylus">
-h3
-  margin 40px 0 0
-
-ul
-  list-style-type none
-  padding 0
-
-li
-  display inline-block
-  margin 0 10px
-
-a
-  color #42b983
-</style>
---
+++ src/server/index.js
@@ -0,0 +1,93 @@
+const http = require("http");
+const path = require("path");
+const fse = require("fs-extra");
+const multiparty = require("multiparty");
+const server = http.createServer();
+// http://nodejs.cn/api/path.html#path_path_resolve_paths
+const UPLOAD_DIR = path.resolve(__dirname, "..", "target"); // 大文件存储目录
+
+const resolvePost = req =>
+  new Promise(resolve => {
+    let chunk = ''
+    req.on('data', (data) => {
+      chunk += data
+    })
+    req.on('end', () => {
+      resolve(JSON.parse(chunk))
+    })
+  })
+
+const pipeStream = (path, writeStream) =>
+  new Promise(resolve => {
+    const readStream = fse.createReadStream(path);
+    readStream.on("end", () => {
+      fse.unlinkSync(path);
+      resolve();
+    });
+    readStream.pipe(writeStream);
+  });
+
+// 合并切片
+const mergeFileChunk = async (filePath, fileName, size) => {
+  // + 222 避免用**.jpg作为文件夹
+  const chunkDir = path.resolve(UPLOAD_DIR, fileName + '222')
+  const chunkPaths = await fse.readdir(chunkDir)
+  // 根据切片下标进行排序
+  chunkPaths.sort((a, b) => a.split('-')[0] - b.split('-')[0])
+  await Promise.all(
+    chunkPaths.map((chunkPath, index) => {
+      return pipeStream(
+        path.resolve(chunkDir, chunkPath),
+        // 指定位置创建可写流
+        fse.createWriteStream(filePath, {
+          start: Math.floor(index * size),
+          end: Math.floor((index + 1) * size)
+        })
+      )
+    }
+    )
+  )
+  fse.rmdirSync(chunkDir); // 合并后删除保存切片的目录
+}
+
+server.on("request", async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    res.status = 200;
+    res.end();
+    return;
+  }
+  const multipart = new multiparty.Form()
+  multipart.parse(req, async (err, fields, files) => {
+    if (err) return;
+    console.log("files...", files, fields)
+    const [chunk] = files.chunk
+    const [hash] = fields.hash
+    const [filename] = fields.filename
+    const chunkDir = path.resolve(UPLOAD_DIR, filename + '222')
+    // 切片目录不存在，创建切片目录
+    if (!fse.existsSync(chunkDir)) {
+      await fse.mkdirs(chunkDir);
+    }
+    await fse.move(chunk.path, `${chunkDir}/${hash}`);
+    res.end("received file chunk");
+  })
+  console.log('req.url', req.url)
+  if (req.url === "/merge") {
+    const data = await resolvePost(req);
+    const { filename, size } = data;
+    const filePath = path.resolve(UPLOAD_DIR, `${filename}`);
+    await mergeFileChunk(filePath, filename, size);
+    res.end(
+      JSON.stringify({
+        code: 0,
+        message: "file merged success"
+      })
+    );
+   }
+});
+
+
+
+server.listen(3000, () => console.log("正在监听 3000 端口"));
